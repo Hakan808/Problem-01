14b3c6d713fd73e66ebdf607a5d5f97d84c88144
Hakan808 <hakan.alptekin03@gmail.com>
2025-08-18 21:07:16 +0300

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <p className='mt-10 text-center text-sm text-gray-500'>
+          Üye değil misiniz?{' '}
+          <a
+            href='#'
+            className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
+          >
+            14 günlük ücretsiz deneme başlatın
+          </a>
+        </p>
+    </>
+  )
+}
+
+export default Footer
